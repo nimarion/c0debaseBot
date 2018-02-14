07e319b94c201178bb16a803eb8925179f29d078
Biospheere <biosphere.dev@gmx.de>
2018-02-14 15:21:27 +0100

    Created Constants

---
+++ src/main/java/de/c0debase/bot/utils/Constants.java
@@ -0,0 +1,16 @@
+package de.c0debase.bot.utils;
+
+import com.google.gson.Gson;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Biosphere
+ * @date 14.02.18
+ */
+public class Constants {
+
+    public static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    public static final Gson gson = new Gson();
+
+}
