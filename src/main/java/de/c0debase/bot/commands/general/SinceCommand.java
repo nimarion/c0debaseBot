f9b2f1873f82c3dab6f276db8232948d6370ae6b
Biospheere <biosphere.dev@gmx.de>
2018-03-24 15:05:54 +0100

    Created SinceCommand

---
+++ src/main/java/de/c0debase/bot/commands/general/SinceCommand.java
@@ -0,0 +1,26 @@
+package de.c0debase.bot.commands.general;
+
+import de.c0debase.bot.commands.Command;
+import net.dv8tion.jda.core.entities.Message;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * @author Biosphere
+ * @date 24.03.18
+ */
+public class SinceCommand extends Command {
+
+    public SinceCommand() {
+        super("since", "Zeigt wie lang du schon auf dem Server bist", Categorie.GENERAL);
+    }
+
+    @Override
+    public void execute(String[] args, Message msg) {
+        msg.getTextChannel().sendMessage(getEmbed(msg.getGuild(), msg.getAuthor())
+                .setDescription("Du bist seit " + ChronoUnit.DAYS.between(msg.getMentionedMembers().stream().findFirst().orElse(msg.getMember()).getJoinDate(), LocalDateTime.now().atOffset(ZoneOffset.UTC)) + " Tagen auf " + msg.getGuild().getName())
+                .build()).queue();
+    }
+}
