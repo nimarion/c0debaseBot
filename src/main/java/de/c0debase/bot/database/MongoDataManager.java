200554a170b1478e020e453ef93f5561068f7903
Biospheere <biosphere.dev@gmx.de>
2018-06-25 18:46:01 +0200

    Calculate coins

--- src/main/java/de/c0debase/bot/database/MongoDataManager.java
+++ src/main/java/de/c0debase/bot/database/MongoDataManager.java
@@ -58,12 +58,16 @@ public class MongoDataManager {
                 levelUser.setGuildID(guildID);
                 levelUser.setLevel(0);
                 levelUser.addXP(50);
+                levelUser.setCoins(levelUser.getXp() * 0.05);
                 levelUser.setLastMessage(System.currentTimeMillis());
                 levelUser.setUserID(userID);
                 levelUser.setRoles(new ArrayList<>());
                 mongoDatabaseManager.getUsers().insertOne(Document.parse(Constants.GSON.toJson(levelUser)));
             } else {
                 levelUser = Constants.GSON.fromJson(document.toJson(jsonWriterSettings), LevelUser.class);
+                if(levelUser.getCoins() == null){
+                    levelUser.setCoins(levelUser.getXp() * 0.01);
+                }
             }
             userCache.put(guildID + "-" + userID, levelUser);
             consumer.accept(levelUser);
