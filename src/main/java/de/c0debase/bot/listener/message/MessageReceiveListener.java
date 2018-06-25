9d30d48cef282fce345ae0d1bfb5520e5320d2d8
Niklas <23435250+Biospheere@users.noreply.github.com>
2018-06-26 00:38:21 +0200

    Development (#64)
    * Add reactions in poll channel
    
    * Removed permission check
    
    * Created GithubCommand
    
    * Added permission check for staff group
    
    * Dont display image
    
    * Set last join date
    
    * Display invite message for instant invites
    
    * Changed language
    
    * Added clone Field
    
    * Give xp with instant invite
    
    * Changed emote name
    
    * Created RoleStatsCommand
    
    * Sort roles by member size
    
    * Created EvalCommand
    
    * Added HikariConfig and HikariPool
    
    * Added HikariCP
    
    * Update README.md
    
    * Created BeLikeBillCommand
    
    * Improve role-stats-command performance (#50)
    
    * Mention target
    
    * Use Nickname
    
    * Fix equals sign (#53)
    
    (╯°□°）╯︵ ┻━┻
    
    * Mongodb (#56)
    
    * Created UserinfoCommand
    
    * Added Gson
    
    * Added MongoDB and ExpiringMap
    
    * Deletet CheckCommand
    
    * Dont add Projekt Role
    
    * Added MongoDB stuff
    
    * Created ImportDataCommand
    
    * Deletet LevelUser
    
    * Removed load methode
    
    * Removed Generics
    
    * Give roles automatically on rejoin //Not testet yet
    
    * Development (#54)
    
    * Mention target
    
    * Use Nickname
    
    * Removed HikariCP and MySql Connector dependencies
    
    * Added org.mongodb.driver
    
    * Removed MonitorManager
    
    * Removed InviteChecker
    
    * Check for page
    
    * Check if role is null
    
    * Add Id instead of name
    
    * Changed collection name
    
    * Added missing return
    
    * Deletet some classes
    
    * Dont send message
    
    * Added MONGO-HOST key
    
    * A hopeful fix for the dynamic channel-managemant-system (#58)
    
    * Add license 1 (#59)
    
    * Development (#57)
    
    * Add reactions in poll channel
    
    * Removed permission check
    
    * Created GithubCommand
    
    * Added permission check for staff group
    
    * Dont display image
    
    * Set last join date
    
    * Display invite message for instant invites
    
    * Changed language
    
    * Added clone Field
    
    * Give xp with instant invite
    
    * Changed emote name
    
    * Created RoleStatsCommand
    
    * Sort roles by member size
    
    * Created EvalCommand
    
    * Added HikariConfig and HikariPool
    
    * Added HikariCP
    
    * Update README.md
    
    * Created BeLikeBillCommand
    
    * Improve role-stats-command performance (#50)
    
    * Mention target
    
    * Use Nickname
    
    * Fix equals sign (#53)
    
    (╯°□°）╯︵ ┻━┻
    
    * Mongodb (#56)
    
    * Created UserinfoCommand
    
    * Added Gson
    
    * Added MongoDB and ExpiringMap
    
    * Deletet CheckCommand
    
    * Dont add Projekt Role
    
    * Added MongoDB stuff
    
    * Created ImportDataCommand
    
    * Deletet LevelUser
    
    * Removed load methode
    
    * Removed Generics
    
    * Give roles automatically on rejoin //Not testet yet
    
    * Development (#54)
    
    * Mention target
    
    * Use Nickname
    
    * Removed HikariCP and MySql Connector dependencies
    
    * Added org.mongodb.driver
    
    * Removed MonitorManager
    
    * Removed InviteChecker
    
    * Check for page
    
    * Check if role is null
    
    * Add Id instead of name
    
    * Changed collection name
    
    * Added missing return
    
    * Deletet some classes
    
    * Dont send message
    
    * Added MONGO-HOST key
    
    * Create LICENSE
    
    * Use ArrayList
    
    * changed levels to users
    
    * Set empty ArrayList
    
    * Delete dynamic voice channel system temporarily (#60)
    
    * Ignore ReadyListener
    
    * Update README.md
    
    * Deleted docker-compose.yml
    
    * Created LennyCommand
    
    * Improved RoleCommand
    
    * Add  Coins
    
    * Calculate coins
    
    * Changed Title
    
    * Added Random
    
    * Created CoinCommand
    
    * ¯\_(ツ)_/¯
    
    * Fix NPE
    
    * More Coins ;)
    
    * Added Antispam
    
    * Added return
    
    * Changed ExpiringMap settings
    
    * Added some funny gifs ^^

--- src/main/java/de/c0debase/bot/listener/message/MessageReceiveListener.java
+++ src/main/java/de/c0debase/bot/listener/message/MessageReceiveListener.java
@@ -5,12 +5,20 @@ import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import com.vdurmont.emoji.EmojiManager;
 import de.c0debase.bot.CodebaseBot;
+import de.c0debase.bot.utils.Constants;
+import de.c0debase.bot.utils.Pagination;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.jodah.expiringmap.ExpiringMap;
 
 import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Biosphere
@@ -19,11 +27,36 @@ import java.awt.*;
 
 public class MessageReceiveListener extends ListenerAdapter {
 
+    private final Map<Member, String> lastMessage;
+    private final List<String> gifs = Arrays.asList(
+            "https://media.giphy.com/media/5VKbvrjxpVJCM/giphy.gif",
+            "https://media.giphy.com/media/4cUCFvwICarHq/giphy.gif",
+            "https://media.giphy.com/media/1ym5LJ17vp77BL8X5O/giphy.gif",
+            "https://media.giphy.com/media/KI9oNS4JBemyI/giphy.gif",
+            "https://media.giphy.com/media/l1CC9FjH54QhYHExq/source.gif",
+            "https://media.giphy.com/media/2gYhkl6mLIYZxpMve1/giphy.gif",
+            "https://media.giphy.com/media/kmU72Ms75Zhlu/giphy.gif",
+            "https://media.giphy.com/media/xHMIDAy1qkzNS/giphy.gif",
+            "https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif",
+            "https://media.giphy.com/media/cbb8zL5wbNnfq/giphy.gif",
+            "https://media.giphy.com/media/aLdiZJmmx4OVW/giphy.gif",
+            "https://media.giphy.com/media/qPcX2mzk3NmjC/giphy.gif",
+            "https://media.giphy.com/media/kjCFOUT3ZIlAA/giphy.gif",
+            "https://media.giphy.com/media/ZisaVxhbs1iDK/giphy.gif"
+    );
+
+    public MessageReceiveListener(){
+        final ExpiringMap.Builder<Object, Object> mapBuilder = ExpiringMap.builder();
+        mapBuilder.expiration(30, TimeUnit.SECONDS).build();
+        lastMessage = mapBuilder.build();
+    }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) {
             return;
         }
+
         if (event.isFromType(ChannelType.PRIVATE)) {
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setColor(Color.RED);
@@ -77,12 +110,19 @@ public class MessageReceiveListener extends ListenerAdapter {
         if (event.getMessage().getTextChannel().getName().equalsIgnoreCase("bot") && event.getMessage().getContentRaw().startsWith("!")) {
             CodebaseBot.getInstance().getCommandManager().execute(event.getMessage());
         } else {
+            if(lastMessage.containsKey(event.getMember()) && lastMessage.get(event.getMember()).equalsIgnoreCase(event.getMessage().getContentRaw())){
+                event.getMessage().delete().queue();
+                return;
+            } else {
+                lastMessage.put(event.getMember(), event.getMessage().getContentRaw());
+            }
             CodebaseBot.getInstance().getMongoDataManager().getLevelUser(event.getGuild().getId(), event.getAuthor().getId(), levelUser -> {
                 float time = (System.currentTimeMillis() - levelUser.getLastMessage()) / 1000;
                 if (time >= 50.0f) {
                     if (levelUser.addXP(50)) {
                         EmbedBuilder levelUpEmbed = new EmbedBuilder();
                         levelUpEmbed.appendDescription(event.getAuthor().getAsMention() + " ist nun Level " + levelUser.getLevel());
+                        levelUpEmbed.setImage(gifs.get(Constants.RANDOM.nextInt(gifs.size())));
                         event.getTextChannel().sendMessage(levelUpEmbed.build()).queue();
                     }
                     levelUser.setLastMessage(System.currentTimeMillis());
