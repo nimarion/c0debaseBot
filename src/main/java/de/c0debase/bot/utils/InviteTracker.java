fea91a8cfef98cf1a005d7c0da72b9c4f7f8666c
niklas <23435250+Biospheere@users.noreply.github.com>
2019-02-10 19:00:12 +0100

    created InviteTracker

---
+++ src/main/java/de/c0debase/bot/utils/InviteTracker.java
@@ -0,0 +1,44 @@
+package de.c0debase.bot.utils;
+
+import de.c0debase.bot.core.Codebase;
+import de.c0debase.bot.database.data.CodebaseUser;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Invite;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class InviteTracker {
+
+    private final Codebase bot;
+    private final ConcurrentHashMap<String, Invite> inviteHashMap;
+
+    public InviteTracker(final Codebase bot) {
+        this.bot = bot;
+        inviteHashMap = new ConcurrentHashMap<>();
+    }
+
+    public void start() {
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> bot.getJDA().getGuilds().get(0).getInvites().queue(inviteList -> inviteList.forEach(invite -> {
+            if (inviteHashMap.containsKey(invite.getCode()) && invite.getUses() > inviteHashMap.get(invite.getCode()).getUses()) {
+                final CodebaseUser levelUser = bot.getDataManager().getUserData(invite.getGuild().getId(), invite.getInviter().getId());
+
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setDescription(invite.getInviter().getAsMention() + " vielen Dank das du jemand neues auf c0debase gebracht hast [" + invite.getCode() + "]");
+
+                bot.getJDA().getGuilds().get(0).getTextChannels().get(0).sendMessage(invite.getInviter().getAsMention() + " " + invite.getCode()).queue();
+
+                bot.getJDA().getTextChannelById(System.getenv("BOTCHANNEL")).sendMessage(embedBuilder.build()).queue();
+
+                if (levelUser.addXP(100)) {
+                    EmbedBuilder levelUpEmbed = new EmbedBuilder();
+                    levelUpEmbed.setDescription(invite.getInviter().getAsMention() + " ist nun Level " + levelUser.getLevel());
+                    bot.getJDA().getTextChannelById(System.getenv("BOTCHANNEL")).sendMessage(levelUpEmbed.build()).queue();
+                }
+                bot.getDataManager().updateUserData(levelUser);
+            }
+            inviteHashMap.put(invite.getCode(), invite);
+        })), 5, 5, TimeUnit.SECONDS);
+    }
+}
