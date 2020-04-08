78901cd8a0ed21f17067a8adf32238a211472ae6
niklas <23435250+Biospheere@users.noreply.github.com>
2020-04-08 14:17:45 +0200

    :sparkles: Added Tag Command

---
+++ src/main/java/de/c0debase/bot/tags/Tag.java
@@ -0,0 +1,59 @@
+package de.c0debase.bot.tags;
+
+import java.util.List;
+
+public class Tag {
+
+    private String name;
+    private String title;
+    private String url;
+    private String description;
+    private List<CustomField> fields;
+
+    public Tag() {
+
+    }
+
+    public List<CustomField> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<CustomField> fields) {
+        this.fields = fields;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    
+
+}
