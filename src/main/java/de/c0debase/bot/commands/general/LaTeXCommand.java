12288e55a8983599f7e5c217bbaf9e2141721717
niklas <23435250+Biospheere@users.noreply.github.com>
2020-07-24 14:25:28 +0200

    refactor: use static Gson constant

--- src/main/java/de/c0debase/bot/commands/general/LaTeXCommand.java
+++ src/main/java/de/c0debase/bot/commands/general/LaTeXCommand.java
@@ -2,6 +2,7 @@ package de.c0debase.bot.commands.general;
 
 import com.google.gson.Gson;
 import de.c0debase.bot.commands.Command;
+import de.c0debase.bot.utils.Constants;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import okhttp3.*;
@@ -38,47 +39,35 @@ public class LaTeXCommand extends Command {
     }
 
     @Override
-    public void execute(String[] args, Message message) {
+    public void execute(final String[] args, final Message message) {
         // Define the LaTeX expression
-        String expression = String.join(" ", args).trim();
+        final String expression = String.join(" ", args).trim();
 
         // Check of the expression is empty
         if (expression.isEmpty()) {
-            MessageEmbed embed = getEmbed(message.getGuild(), message.getAuthor())
-                    .setTitle("Fehler")
-                    .setColor(Color.RED)
-                    .setDescription("Die LaTeX expression darf nicht leer sein!")
-                    .build();
+            final MessageEmbed embed = getEmbed(message.getGuild(), message.getAuthor()).setTitle("Fehler")
+                    .setColor(Color.RED).setDescription("Die LaTeX expression darf nicht leer sein!").build();
             message.getTextChannel().sendMessage(embed).queue();
             return;
         }
 
         // Render the expression
-        MessageEmbed embed = getEmbed(message.getGuild(), message.getAuthor())
-                .setTitle("Rendert...")
-                .setColor(Color.YELLOW)
-                .setDescription("Die Expression wird gerendert...")
-                .build();
+        final MessageEmbed embed = getEmbed(message.getGuild(), message.getAuthor()).setTitle("Rendert...")
+                .setColor(Color.YELLOW).setDescription("Die Expression wird gerendert...").build();
         message.getTextChannel().sendMessage(embed).queue(msg -> {
             // Retrieve the image URL
             renderLaTeX(expression).whenComplete((imageURL, throwable) -> {
                 // Check if an error occurred
                 if (throwable != null) {
-                    MessageEmbed errorEmbed = getEmbed(message.getGuild(), message.getAuthor())
-                            .setTitle("Fehler")
-                            .setColor(Color.RED)
-                            .setDescription("`" + throwable.getMessage() + "`")
-                            .build();
+                    final MessageEmbed errorEmbed = getEmbed(message.getGuild(), message.getAuthor()).setTitle("Fehler")
+                            .setColor(Color.RED).setDescription("`" + throwable.getMessage() + "`").build();
                     msg.editMessage(errorEmbed).queue();
                     return;
                 }
 
                 // Respond with the image
-                MessageEmbed successEmbed = getEmbed(message.getGuild(), message.getAuthor())
-                        .setTitle("LaTeX-Expression")
-                        .setColor(Color.GREEN)
-                        .setImage(imageURL)
-                        .build();
+                final MessageEmbed successEmbed = getEmbed(message.getGuild(), message.getAuthor())
+                        .setTitle("LaTeX-Expression").setColor(Color.GREEN).setImage(imageURL).build();
                 msg.editMessage(successEmbed).queue();
             });
         });
@@ -95,46 +84,41 @@ public class LaTeXCommand extends Command {
         expression = LATEX_TEMPLATE.replace("#CONTENT#", expression);
 
         // Define the completable future to use during the process
-        CompletableFuture<String> future = new CompletableFuture<>();
+        final CompletableFuture<String> future = new CompletableFuture<>();
 
         // Define the request JSON values
-        Map<String, Object> requestValues = Map.of(
-                "code", expression,
-                "format", "png",
-                "quality", 100,
-                "density", 400
-        );
+        final Map<String, Object> requestValues = Map.of("code", expression, "format", "png", "quality", 100, "density",
+                400);
 
         // Build a HTTP request
-        Request request = new Request.Builder()
-                .url(RTEX_URL)
-                .method("POST", RequestBody.create(MediaType.parse("application/json"), new Gson().toJson(requestValues)))
+        final Request request = new Request.Builder().url(RTEX_URL)
+                .method("POST",
+                        RequestBody.create(MediaType.parse("application/json"), new Gson().toJson(requestValues)))
                 .build();
 
         // Perform the HTTP request
-        httpClient.newCall(request)
-                .enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException exception) {
-                        future.completeExceptionally(exception);
-                    }
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException exception) {
+                future.completeExceptionally(exception);
+            }
 
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        // Parse the response JSON values
-                        String body = response.body().string();
-                        Map<String, Object> responseValues = new Gson().fromJson(body, Map.class);
+            @Override
+            public void onResponse(final Call call, final Response response) throws IOException {
+                // Parse the response JSON values
+                final String body = response.body().string();
+                final Map<String, Object> responseValues = Constants.GSON.fromJson(body, Map.class);
 
-                        // Complete the future exceptionally if an error occurred
-                        if ("error".equals(responseValues.get("status"))) {
-                            future.completeExceptionally(new Exception((String) responseValues.get("description")));
-                            return;
-                        }
+                // Complete the future exceptionally if an error occurred
+                if ("error".equals(responseValues.get("status"))) {
+                    future.completeExceptionally(new Exception((String) responseValues.get("description")));
+                    return;
+                }
 
-                        // Complete the future with the image URL
-                        future.complete(RTEX_URL + "/" + responseValues.get("filename"));
-                    }
-                });
+                // Complete the future with the image URL
+                future.complete(RTEX_URL + "/" + responseValues.get("filename"));
+            }
+        });
 
         // Return the future
         return future;
